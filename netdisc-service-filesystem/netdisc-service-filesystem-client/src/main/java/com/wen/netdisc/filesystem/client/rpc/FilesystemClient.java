809787a6b4c33e81153c34314b6ba028b14d1f79
calwen <2280400645@qq.com>
2022-09-16 18:47:29 +0800

    [fix] 仅公开application-demo.yml 模板配置文件。

--- netdisc-service-filesystem/netdisc-service-filesystem-client/src/main/java/com/wen/netdisc/filesystem/client/rpc/FilesystemClient.java
+++
@@ -1,9 +0,0 @@
-package com.wen.netdisc.filesystem.client.rpc;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(value = "netdisc-filesystem", path = "/rpc/filesystems")
-//@FeignClient(value = "netdisc-filesystem-dev", path = "/rpc/filesystems")
-public interface FilesystemClient extends FileResource {
-
-}
